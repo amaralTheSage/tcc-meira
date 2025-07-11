57e3a36ccafe27ca44741ba6ee1310048828eb45
amaralTheSage <amaraldesouza9@gmail.com>
2025-07-10 21:03:16 -0300

    Adds homepage notification panel

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,11 +1,12 @@
 import { AddProjectDialog } from '@/components/home/add-project-dialog';
+import HomeNotificationMenu from '@/components/home/home-notification-menu';
 import HomeProjectCard from '@/components/home/home-project-card';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { User, type SharedData } from '@/types';
 import { Project } from '@/types/models';
 import { Head, Link, usePage } from '@inertiajs/react';
-import { Bell, Globe, Plus } from 'lucide-react';
+import { Globe, Plus } from 'lucide-react';
 
 export default function Home({ projects, users }: { projects: Project[]; users: User[] }) {
     const { auth } = usePage<SharedData>().props;
@@ -48,10 +49,7 @@ export default function Home({ projects, users }: { projects: Project[]; users: 
                         <div>
                             <div className="flex justify-between px-3">
                                 <h1 className="mb-2 text-xl font-medium">Seus Projetos</h1>
-                                <div className="relative cursor-pointer">
-                                    <Bell />
-                                    <div className="absolute top-0.5 right-0 size-2.5 rounded-full bg-red-400 shadow-md"></div>
-                                </div>
+                                <HomeNotificationMenu />
                             </div>
 
                             <ul className="">
