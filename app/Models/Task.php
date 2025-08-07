2e4fe01bb4947847efa5ed241e4c777411ed9b9b
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2025-08-06 22:58:59 -0300

    UPDATE: add relationships to subtasks(HasMany) and Collumns(BelongsTo)

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,17 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['id', 'title', 'image', 'x', 'y', 'project_id'];
+    protected $fillable = ['id', 'title', 'image', 'x', 'y','collumn_id', 'project_id', 'position'];
 
     public $incrementing = false;
 
     protected $keyType = 'string';
+
+    public function subtasks(): HasMany
+    {
+        return $this->hasMany(Subtask::class);
+    }
+
+    public function collumn()
+    {
+        return $this->belongsTo(Collumn::class);
+    }
 
     public function project()
     {
