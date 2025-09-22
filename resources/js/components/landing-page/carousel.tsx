4e648601812b5d4d38b06ee5b9c7bbdab8885f5f
amaralTheSage <amaraldesouza9@gmail.com>
2025-09-22 20:00:29 -0300

    Updates landing page carousel for the presentation

--- resources/js/components/landing-page/carousel.tsx
+++ resources/js/components/landing-page/carousel.tsx
@@ -3,7 +3,7 @@
 import Autoplay from 'embla-carousel-autoplay';
 import * as React from 'react';
 
-import { Card, CardContent } from '@/components/ui/card';
+import { Card } from '@/components/ui/card';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 
 export function LandingCarousel() {
@@ -25,12 +25,10 @@ export function LandingCarousel() {
                     {Array.from({ length: 5 }).map((_, index) => (
                         <CarouselItem key={index} className="basis-[78%] pl-4 transition-transform duration-300 ease-out">
                             <Card className="relative rounded-xl shadow-xl">
-                                <CardContent className="flex aspect-[18/9] items-center justify-center p-6">
-                                    <span className="text-4xl font-semibold text-white">{index + 1}</span>
-                                </CardContent>
+                                <img src="/landing-carousel/traceboard.png" alt="" className="h-full" />
 
-                                <div className="absolute right-0 bottom-0 w-lg rounded-tl-2xl bg-[rgba(52,39,39,0.5)] p-5 text-[#d7bebe]">
-                                    <p className="mb-2 text-lg text-[#eae0e0] italic">Junte-se aos puto q são aqueles teus amigo</p>
+                                <div className="absolute right-0 bottom-0 w-lg rounded-tl-2xl bg-[#221a1a] p-5 text-[#d7bebe]">
+                                    <p className="mb-2 text-lg text-[#eae0e0] italic">Get in With the Squad</p>
                                     <p className="text-sm">
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. At expedita accusamus enim quod perferendis
                                         consequuntur hic.
