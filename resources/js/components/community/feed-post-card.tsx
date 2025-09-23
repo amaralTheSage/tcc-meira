b5e1e3861ad59e413508ab63b7db50f2c2536501
amaralTheSage <amaraldesouza9@gmail.com>
2025-09-23 10:33:55 -0300

    Adds visuals to the Community front page (feed)

---
+++ resources/js/components/community/feed-post-card.tsx
@@ -0,0 +1,70 @@
+import { useInitials } from '@/hooks/use-initials';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+
+export interface FeedPostInterface {
+    img: string;
+    title: string;
+    description: string;
+    size: 'L' | 'S';
+}
+
+const members = [
+    { id: 1, avatar: null, name: 'Marce1in Passa Piça' },
+    { id: 2, avatar: null, name: 'Marce1in Passa Piça' },
+    { id: 3, avatar: null, name: 'Marce1in Passa Piça' },
+    { id: 4, avatar: null, name: 'Marce1in Passa Piça' },
+];
+
+export default function FeedPostCard({ post }: { post: FeedPostInterface }) {
+    const getInitials = useInitials();
+
+    return (
+        <li className={`flex flex-col overflow-hidden rounded-xl bg-neutral-900 shadow-lg ${post.size === 'L' && 'col-span-2'}`}>
+            <div className="h-48 w-full overflow-hidden">
+                <img src={post.img} alt={post.title} className="h-full w-full object-cover" />
+            </div>
+
+            {/* Content */}
+            <div className="flex flex-col gap-3 p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-cardo text-3xl text-nowrap text-white italic">{post.title}</h2>
+
+                    <div className="flex items-center gap-2">
+                        <div className="flex -space-x-5 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:ring-background">
+                            {members &&
+                                members.map((member: any) => (
+                                    <div className="flex w-fit">
+                                        <Avatar key={member.id}>
+                                            <AvatarImage src={member.avatar} alt={member.name} className="object-cover" />
+                                            <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                                {getInitials(member.name)}
+                                            </AvatarFallback>
+                                        </Avatar>
+                                    </div>
+                                ))}
+                        </div>
+                        {post.size === 'L' && (
+                            <span className="max-w-[175px] text-sm max-md:hidden">
+                                {members[0].name.split(' ')[0]} and {members.length - 1} others
+                            </span>
+                        )}
+                    </div>
+                </div>
+
+                {post.size === 'S' && (
+                    <p className="font-cardo text-md text-neutral-400">
+                        {post.description.length > 93 ? post.description.slice(0, 93) + '... ' : post.description}
+                        <button className="text-sm text-white hover:underline">read more</button>
+                    </p>
+                )}
+
+                {post.size === 'L' && (
+                    <p className="font-cardo text-md text-neutral-400">
+                        {post.description.length > 245 ? post.description.slice(0, 245) + '... ' : post.description}
+                        <button className="text-sm text-white hover:underline">read more</button>
+                    </p>
+                )}
+            </div>
+        </li>
+    );
+}
--- resources/js/pages/community/feed.tsx
+++ resources/js/pages/community/feed.tsx
@@ -1,3 +1,4 @@
+import FeedPostCard, { FeedPostInterface } from '@/components/community/feed-post-card';
 import AppLayoutTemplate from '@/layouts/app/app-header-layout';
 import { BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -10,10 +11,91 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+const testData: FeedPostInterface[] = [
+    {
+        img: 'https://picsum.photos/600/400?random=1',
+        title: '5ª SAJIC',
+        description:
+            'Plataforma desenvolvida para a Semana Acadêmica de Jornalismo e Inovação Científica 2025. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'L',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=2',
+        title: '4ª SAJIC',
+        description:
+            'Projeto colaborativo da edição anterior do evento, focado em inovação tecnológica e acadêmica. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'S',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=3',
+        title: 'Coisa Imóveis',
+        description:
+            'Plataforma de busca de imóveis criada para simular listagens de casas e apartamentos. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'S',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=4',
+        title: 'MEIRA',
+        description:
+            'Ferramenta de gerenciamento de projetos simplificada para equipes acadêmicas e startups. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'S',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=5',
+        title: 'Portfólio Acadêmico',
+        description:
+            'Aplicação criada para exibir projetos e colaborações realizados em 2025. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'L',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=6',
+        title: 'Demo Imobiliária',
+        description:
+            'Exemplo de listagem de propriedades com interface intuitiva e responsiva. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'S',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=2',
+        title: '4ª SAJIC',
+        description:
+            'Projeto colaborativo da edição anterior do evento, focado em inovação tecnológica e acadêmica. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'L',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=3',
+        title: 'Coisa Imóveis',
+        description:
+            'Plataforma de busca de imóveis criada para simular listagens de casas e apartamentos. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'S',
+    },
+    {
+        img: 'https://picsum.photos/600/400?random=4',
+        title: 'MEIRA',
+        description:
+            'Ferramenta de gerenciamento de projetos simplificada para equipes acadêmicas e startups. Sed tempus sagittis lacus eget blandit. Morbi ut hendrerit tellus. Aliquam eget posuere ipsum, a viverra lectus. Aliquam erat volutpat. Mauris luctus justo vel felis auctor faucibus. Ut sollicitudin massa eget interdum ornare. ',
+        size: 'S',
+    },
+];
+
 export default function Feed() {
     return (
         <AppLayoutTemplate breadcrumbs={breadcrumbs}>
             <Head title="Community" />
+
+            <div className="mt-20 mb-8 space-y-4">
+                <h2 className="font-cardo text-4xl font-medium tracking-tight">Community Projects</h2>
+                <nav className="space-x-4">
+                    <span className="text-2xl underline">Everyone</span>
+                    <span className="text-2xl text-gray-400">Friends</span>
+                </nav>
+            </div>
+
+            <ul className="grid grid-cols-4 gap-4">
+                {testData.map((post) => {
+                    return <FeedPostCard post={post} />;
+                })}
+            </ul>
+
             <Toaster />
         </AppLayoutTemplate>
     );
