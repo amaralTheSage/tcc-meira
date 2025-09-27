69daca0e36afa3cf27f0309537c37f661e6fd46b
amaralTheSage <amaraldesouza9@gmail.com>
2025-09-27 14:51:19 -0300

    Adds minor change to feed-post-card

--- resources/js/components/community/feed-post-card.tsx
+++ resources/js/components/community/feed-post-card.tsx
@@ -20,56 +20,42 @@ export default function FeedPostCard({ post }: { post: FeedPostInterface }) {
     const getInitials = useInitials();
 
     return (
-        <li className={`flex flex-col overflow-hidden rounded-xl bg-neutral-900 shadow-lg ${post.size === 'L' && 'col-span-2'}`}>
-            <div className="h-48 w-full overflow-hidden">
-                <img src={post.img} alt={post.title} className="h-full w-full object-cover" />
-            </div>
-
-            {/* Content */}
-            <div className="flex flex-col gap-3 p-4">
-                <div className="flex items-center justify-between">
-                    <h2 className="font-cardo text-3xl text-nowrap text-white italic">{post.title}</h2>
-
-                    <div className="flex items-center gap-2">
-                        <div className="flex -space-x-5 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:ring-background">
-                            {members &&
-                                members.map((member: any) => (
-                                    <div className="flex w-fit">
-                                        <Avatar key={member.id}>
-                                            <AvatarImage src={member.avatar} alt={member.name} className="object-cover" />
-                                            <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                                {getInitials(member.name)}
-                                            </AvatarFallback>
-                                        </Avatar>
-                                    </div>
-                                ))}
-                        </div>
-                        {post.size === 'L' && (
-                            <span className="max-w-[175px] text-sm max-md:hidden">
-                                {members[0].name.split(' ')[0]} and {members.length - 1} others
-                            </span>
-                        )}
-                    </div>
+        <ProjectDialog title={post.title} description={post.description} members={members}>
+            <li className={`flex flex-col overflow-hidden rounded-xl bg-neutral-900 shadow-lg ${post.size === 'L' && 'col-span-2'} cursor-pointer`}>
+                <div className="h-48 w-full overflow-hidden">
+                    <img src={post.img} alt={post.title} className="h-full w-full object-cover" />
                 </div>
 
-                {post.size === 'S' && (
-                    <p className="font-cardo text-md text-neutral-400">
-                        {post.description.length > 93 ? post.description.slice(0, 93) + '... ' : post.description}
-                        <ProjectDialog title={post.title} description={post.description} members={members}>
-                            <button className="cursor-pointer text-sm text-white underline">read more</button>
-                        </ProjectDialog>
-                    </p>
-                )}
+                {/* Content */}
+                <div className="flex flex-col gap-3 p-4">
+                    <div className="flex items-center justify-between">
+                        <h2 className="font-cardo text-3xl text-nowrap text-white italic">{post.title}</h2>
 
-                {post.size === 'L' && (
-                    <p className="font-cardo text-md text-neutral-400">
-                        {post.description.length > 245 ? post.description.slice(0, 245) + '... ' : post.description}
-                        <ProjectDialog title={post.title} description={post.description} members={members}>
-                            <button className="cursor-pointer text-sm text-white underline">read more</button>
-                        </ProjectDialog>
-                    </p>
-                )}
-            </div>
-        </li>
+                        <div className="flex items-center gap-2">
+                            <div className="flex -space-x-5 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:ring-background">
+                                {members &&
+                                    members.map((member: any) => (
+                                        <div className="flex w-fit">
+                                            <Avatar key={member.id}>
+                                                <AvatarImage src={member.avatar} alt={member.name} className="object-cover" />
+                                                <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                                    {getInitials(member.name)}
+                                                </AvatarFallback>
+                                            </Avatar>
+                                        </div>
+                                    ))}
+                            </div>
+                            {post.size === 'L' && (
+                                <span className="max-w-[175px] text-sm max-md:hidden">
+                                    {members[0].name.split(' ')[0]} and {members.length - 1} others
+                                </span>
+                            )}
+                        </div>
+                    </div>
+
+                    <p className="font-cardo text-md line-clamp-3 text-neutral-400">{post.description}</p>
+                </div>
+            </li>
+        </ProjectDialog>
     );
 }
