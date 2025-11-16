b40dd7ed40cd4ffd1887256ccc5fa5e31f1d84de
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2025-11-16 13:41:09 -0300

    FEAT: task type

--- resources/js/types/models.ts
+++ resources/js/types/models.ts
@@ -57,10 +57,20 @@ export interface CommunityPost {
     [key: string]: unknown;
 }
 
+export interface ColumnTask {
+    id: string;
+    title?: string;
+    description?: string;
+    position: number;
+    
+    [key: string]: unknown;
+}
+
 export interface Column {
     id: string;
     name?: string;
     position: number;
+    tasks?: ColumnTask[];
 
     [key: string]: unknown;
 }
