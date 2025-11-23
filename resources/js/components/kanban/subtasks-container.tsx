a6f751b2958c1d27f7a44c0351efa56d41ced292
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2025-11-23 01:25:25 -0300

    FIX: add connections to subtasks

--- resources/js/components/kanban/subtasks-container.tsx
+++ resources/js/components/kanban/subtasks-container.tsx
@@ -2,9 +2,13 @@ import { TaskSubtask } from "@/types/models";
 
 export default function SubtaskContainer({ subtask }: { subtask: TaskSubtask }) {
     return (
-        <div className="ml-6 min-h-5 max-w-10/12 cursor-pointer bg-neutral-700 hover:border-solid border-solid border-neutral-500 border-2 duration-75 hover:border-red-700 w-full rounded-md mb-0.5 p-1.5">
-            <div className="w-full flex items-center justify-between">
-                <span className="truncate px-2.5">{subtask.title || "Untitled Task"}</span>
+        <div className="relative flex items-center mb-0.5 pl-4 z-0">
+            <div className="absolute left-2 top-3/5 h-0.5 w-2 bg-neutral-500 transform -translate-y-1/2"></div> {/* horizontal line */}
+            <div className="absolute left-2 -top-5 h-11 w-0.5 bg-neutral-500"></div> {/* vertical line pointing right */}
+            <div className="min-h-5 max-w-11/12 cursor-pointer bg-neutral-700 hover:border-solid border-solid border-neutral-500 border-2 duration-75 hover:border-red-700 w-full rounded-md p-1.5 flex items-center">
+                <div className="w-full flex items-center justify-between pl-2">
+                    <span className="truncate">{subtask.title || "Untitled Task"}</span>
+                </div>
             </div>
         </div>
     );
