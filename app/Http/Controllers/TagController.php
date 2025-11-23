7ed3f3e4ea3efb62e344493a4e1df1d54eb00779
amaralTheSage <amaraldesouza9@gmail.com>
2025-11-23 11:24:44 -0300

    Adds models, migrations, relationships, and controller for task Tags

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Project $project)
+    {
+        return $project->tags();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Project $project)
+    {
+        $validated = $request->validate(['name' => 'required|string|max:80']);
+
+        Tag::create(['name' => $validated, 'project_id' => $project['id']]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        $validated = $request->validate(['name' => 'required|string|max:80']);
+
+        $tag->update(['name' => $validated['name']]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Tag::where('id', '==', $id)->delete();
+    }
+}
